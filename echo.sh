906ef729a2bb01035c7f34edf955f62c89f2b3a8
Bert Burgemeister <trebbu@googlemail.com>
2015-01-16 22:08:50 +0100

    Fix bit rot in an example shell script

--- echo.sh
+++ echo.sh
@@ -5,6 +5,8 @@ export LC_ALL=C
 FIN=to-g.fifo
 FOUT=from-g.fifo
 
+rm -f $FIN $FOUT
+
 ./pipeglade -i $FIN -o $FOUT &
 
 # wait for $FIN and $FOUT to appear
@@ -12,6 +14,6 @@ while test ! \( -e $FIN -a -e $FOUT \); do :; done
 
 while test -e $FIN -a -e $FOUT; do
     read line <$FOUT
-    echo "textview2:text_view:insert_at_cursor $line\\n" >$FIN
-    echo "textview2:text_view:scroll_to_cursor\\n" >$FIN
+    echo "textview2:insert_at_cursor $line\\n" >$FIN
+    echo "textview2:scroll_to_cursor\\n" >$FIN
 done
