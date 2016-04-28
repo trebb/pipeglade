trebb/pipeglade

commit fa667457a54ca487ef8ee8f4171b616140d9e888

Untangle the fifo() function

pipeglade.c
@@ -125,57 +125,59 @@ ign_cmd(GType type, const char *msg)
 }
 
 /*
+ * Check if n is, or can be made, the name of a fifo.  Give up if n
+ * exists but is not a fifo.
+ */
+static void
+find_fifo(const char *n)
+{
+        struct stat sb;
+
+        stat(n, &sb);
+        if (S_ISFIFO(sb.st_mode)) {
+                if (chmod(n, 0600) != 0)
+                        bye(EXIT_FAILURE, stderr, "using pre-existing fifo %s: %s\n",
+                            n, strerror(errno));
+        } else if (mkfifo(n, 0600) != 0)
+                bye(EXIT_FAILURE, stderr, "making fifo %s: %s\n",
+                    n, strerror(errno));
+}
+
+/*
  * Create a fifo if necessary, and open it.  Give up if the file
  * exists but is not a fifo
  */
 static FILE *
-fifo(const char *name, const char *mode)
+open_in_fifo(const char *name)
 {
-        struct stat sb;
         int fd;
         FILE *s = NULL;
-        int bufmode;
 
-        if (name != NULL) {
-                stat(name, &sb);
-                if (S_ISFIFO(sb.st_mode)) {
-                        if (chmod(name, 0600) != 0)
-                                bye(EXIT_FAILURE, stderr,
-                                    "using pre-existing fifo %s: %s\n",
-                                    name, strerror(errno));
-                } else if (mkfifo(name, 0600) != 0)
-                        bye(EXIT_FAILURE, stderr,
-                            "making fifo %s: %s\n", name, strerror(errno));
-        }
-        switch (mode[0]) {
-        case 'r':
-                bufmode = _IONBF;
-                if (name == NULL)
-                        s = stdin;
-                else {
-                        if ((fd = open(name, O_RDWR | O_NONBLOCK)) < 0)
-                                bye(EXIT_FAILURE, stderr,
-                                    "opening fifo %s (%s): %s\n",
-                                    name, mode, strerror(errno));
-                        s = fdopen(fd, "r");
-                }
-                break;
-        case 'w':
-                bufmode = _IOLBF;
-                if (name == NULL)
-                        s = stdout;
-                else
-                        s = fopen(name, "w+");
-                break;
-        default:
-                ABORT;
-                break;
-        }
-        if (s == NULL)
-                bye(EXIT_FAILURE, stderr, "opening fifo %s (%s): %s\n",
-                    name, mode, strerror(errno));
-        else
-                setvbuf(s, NULL, bufmode, 0);
+        if (name == NULL)
+                return stdin;
+        find_fifo(name);
+        if ((fd = open(name, O_RDWR | O_NONBLOCK)) < 0)
+                bye(EXIT_FAILURE, stderr, "opening fifo %s (r): %s\n",
+                    name, strerror(errno));
+        if ((s = fdopen(fd, "r")) == NULL)
+                bye(EXIT_FAILURE, stderr, "opening fifo %s (r): %s\n",
+                    name, strerror(errno));
+        setvbuf(s, NULL, _IONBF, 0);
+        return s;
+}
+
+static FILE *
+open_out_fifo(const char *name)
+{
+        FILE *s = NULL;
+
+        if (name == NULL)
+                return stdout;
+        find_fifo(name);
+        if ((s = fopen(name, "w+")) == NULL)
+                bye(EXIT_FAILURE, stderr, "opening fifo %s (w): %s\n",
+                    name, strerror(errno));
+        setvbuf(s, NULL, _IOLBF, 0);
         return s;
 }
 
@@ -2735,8 +2737,8 @@ main(int argc, char *argv[])
         if (argv[optind] != NULL)
                 bye(EXIT_FAILURE, stderr,
                     "illegal parameter '%s'\n" USAGE, argv[optind]);
-        in = fifo(in_fifo, "r");
-        out = fifo(out_fifo, "w");
+        in = open_in_fifo(in_fifo);
+        out = open_out_fifo(out_fifo);
         if (bg) {
                 if (in == stdin || out == stdout)
                         bye(EXIT_FAILURE, stderr,

commit b1c4ad9486ef769f3a48aaf755cee8286f372387

Fix regression regarding buffering type of standard streams
- Bug was introduced in fa667457.
- Added a test of buffering type of stdout.

pipeglade.c
@@ -235,14 +235,16 @@ open_in_fifo(const char *name)
         FILE *s = NULL;
 
         if (name == NULL)
-                return stdin;
-        find_fifo(name);
-        if ((fd = open(name, O_RDWR | O_NONBLOCK)) < 0)
-                bye(EXIT_FAILURE, stderr, "opening fifo %s (r): %s\n",
-                    name, strerror(errno));
-        if ((s = fdopen(fd, "r")) == NULL)
-                bye(EXIT_FAILURE, stderr, "opening fifo %s (r): %s\n",
-                    name, strerror(errno));
+                s = stdin;
+        else {
+                find_fifo(name);
+                if ((fd = open(name, O_RDWR | O_NONBLOCK)) < 0)
+                        bye(EXIT_FAILURE, stderr, "opening fifo %s (r): %s\n",
+                            name, strerror(errno));
+                if ((s = fdopen(fd, "r")) == NULL)
+                        bye(EXIT_FAILURE, stderr, "opening fifo %s (r): %s\n",
+                            name, strerror(errno));
+        }
         setvbuf(s, NULL, _IONBF, 0);
         return s;
 }
@@ -253,11 +255,13 @@ open_out_fifo(const char *name)
         FILE *s = NULL;
 
         if (name == NULL)
-                return stdout;
-        find_fifo(name);
-        if ((s = fopen(name, "w+")) == NULL)
-                bye(EXIT_FAILURE, stderr, "opening fifo %s (w): %s\n",
-                    name, strerror(errno));
+                s = stdout;
+        else {
+                find_fifo(name);
+                if ((s = fopen(name, "w+")) == NULL)
+                        bye(EXIT_FAILURE, stderr, "opening fifo %s (w): %s\n",
+                            name, strerror(errno));
+        }
         setvbuf(s, NULL, _IOLBF, 0);
         return s;
 }

pipegladetest.sh
@@ -16,6 +16,7 @@ LOG=test.log
 ERR_FILE=err.txt
 BAD_FIFO=bad_fifo
 PID_FILE=pid
+OUT_FILE=out.txt
 DIR=test_dir
 FILE1=saved1.txt
 FILE2=saved2.txt
@@ -23,7 +24,8 @@ FILE3=saved3.txt
 FILE4=saved4.txt
 FILE5=saved5.txt
 FILE6=saved6.txt
-rm -rf $FIN $FOUT $FERR $LOG $ERR_FILE $BAD_FIFO $PID_FILE $FILE1 $FILE2 $FILE3 $FILE4 $FILE5 $FILE6 $DIR
+rm -rf $FIN $FOUT $FERR $LOG $ERR_FILE $BAD_FIFO $PID_FILE $OUT_FILE \
+   $FILE1 $FILE2 $FILE3 $FILE4 $FILE5 $FILE6 $DIR
 
 if stat -f "%0p"; then
     STAT_CMD='stat -f "%0p"'
@@ -939,10 +941,10 @@ check() {
 }
 
 
-## Logging to stderr while redirecting stderr
+# Logging to stderr while redirecting stderr
 ./pipeglade -i $FIN -o $FOUT -O $ERR_FILE -l - &
 
-# wait for $FIN and $FOUT to appear
+# wait for $FIN to appear
 while test ! \( -e $FIN \); do :; done
 check 0 "" \
       "# Comment"
@@ -954,6 +956,16 @@ check 0 "" \
 check_cmd "grep -qe \"# Comment\" $ERR_FILE"
 check_cmd "grep -qe \"ignoring command\" $ERR_FILE"
 check_rm $FIN
+
+
+# check if stdout remains line buffered even if directed to file
+./pipeglade -i $FIN >$OUT_FILE &
+# wait for $FIN to appear
+while test ! \( -e $FIN \); do :; done
+echo "button1:force" >$FIN
+check_cmd "grep -qe 'button1:clicked' $OUT_FILE"
+echo "_:main_quit" >$FIN
+#rm -f $OUT_FILE
 
 
 ./pipeglade -i $FIN -o $FOUT -b >$PID_FILE